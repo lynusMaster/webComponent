ff7b64582a52a13cea8ed174653bc44e0523b67b
yousub <yousub@socialbean.co.kr>
2022-05-04 14:07:03 +0900

    수정중

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ export default class Header extends Component {
     return `
       <div>
         헤더
+        <div>
+          <span class="history" route="/main">main</span>
+          <span class="history" route="/pll">pll</span>
+        </div>
       </div>
     `
   }
